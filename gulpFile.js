85789775cc9a1220341fa4e536a53ff63a41573c
MU IT Program <muitprogram@MUs-MacBook-Pro-2.local>
2016-05-28 11:37:49 -0500

    Pagespeed Optimizations

--- gulpFile.js
+++ gulpFile.js
@@ -8,40 +8,24 @@ var filter = require('gulp-filter');
 
 var DIST = 'dist/';
 
-gulp.task('build', ['images', 'build--js'], function () {
-    var js = filter(['*', '**/*.js'], {restore: true});
-    var jsApp = filter(['*', '!*.min.js'], {restore: true});
-    var css = filter(['*', '**/*.css'], {restore: true});
-    var cssApp = filter(['*', '!*.min.css'], {restore: true});
-    var index = filter(['*', '**/index.html'], {restore: true});
+gulp.task('build', [], function () {
+    var js = filter(['**/*.js'], {restore: true});
+    var css = filter(['**/*.css'], {restore: true});
+    var index = filter(['**/index.html'], {restore: true});
+    var images = filter(['**/*.(gif|jpg|svg|png)'], {restore: true});
     
     return gulp.src('src/**/*.*')
         .pipe(js)
-        .pipe(jsApp)
         .pipe(uglify())
-        .pipe(jsApp.restore)
         .pipe(js.restore)
         .pipe(css)
-        .pipe(cssApp)
         .pipe(csso())
-        .pipe(cssApp.restore)
         .pipe(css.restore)
         .pipe(index)
         .pipe(inlineSource())
         .pipe(index.restore)
-        .pipe(gulp.dest(DIST));
-});
-
-gulp.task('images', function() {
-   return gulp.src('src/images/**')
-        .pipe(changed(DIST))
+        .pipe(images)
         .pipe(imagemin())
-        .pipe(gulp.dest(DIST));
-});
-
-gulp.task('build--js', function() {
-   return gulp.src('src/lib/js/**')
-        .pipe(changed(DIST))
-        .pipe(imagemin())
+        .pipe(images.restore)
         .pipe(gulp.dest(DIST));
 });
