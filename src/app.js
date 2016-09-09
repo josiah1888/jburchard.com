e83ceacd73dfa9e0f68f8e2fde3b9bdddd288df6
josiah1888 <josiah@jburchard.com>
2016-09-09 10:45:00 -0500

    hotfix(sw): unregister and cache bust sw

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
 'use strict';
 
-$(document).ready(function() {
-    $('.js-toggleDetails').on('click', function() {
+$(document).ready(function () {
+    $('.js-toggleDetails').on('click', function () {
         $(this).next('.js-details').toggle(800);
     });
 
-    $('.js-menuToggle').on('click', function(e) {
+    $('.js-menuToggle').on('click', function (e) {
         e.preventDefault();
 
         $('#sidebar').toggleClass('menu-active');
     });
 });
 
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('/sw.js', { scope: '/' });
-// }
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistration().then(function (r) {
+        r && r.unregister();
+    });
+}
